9433e7f227c3c37a9cd46f2010494c6e35758765
vitoria-vieirac <114622524+vitoria-vieirac@users.noreply.github.com>
2023-04-11 19:42:10 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
+
+
+const convertValues = async () => {
+    const inputReais = document.getElementById("input-real").value
+    const realValueText = document.getElementById("real-value-text")
+    const currencyValueText = document.getElementById("currency-value-text")
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high  
+
+    realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputReais)
+
+
+    if (select.value === "US$ Dólar Americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputReais / dolar)
+    }
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputReais / euro)
+
+    }
+
+
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.getElementById("currency-img")
+
+    if (select.value === "US$ Dólar Americano") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./eua.png"
+    }
+
+    if (select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./euro.png"
+    }
+}
+
+button.addEventListener("click", convertValues)
+select.addEventListener("change", changeCurrency)
